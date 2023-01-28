045b8117501c6c271378429eed60fd7dd93da54d
vorflik <annavorflik@gmail.com>
2023-01-28 18:04:58 +0200

    Hello world Variables creation Cars and Mens Age v2

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
 print("Hello world")
+x = "Hello world!"
+print(x)
+type_of_variable = type(x)
+print(type_of_variable)
+# Assignment 3. Variables creation.
+Car_Model = "sedan"
+Car_Colour = "black"
+Car_Year = "1985"
+Car_Doors_Quantity = "5"
+print(Car_Model + Car_Colour)
+Car_Colour = "white"
+print(Car_Model + Car_Year + Car_Colour + Car_Doors_Quantity)
+Mans_Age = [25, 28, 35, 69, 74]
+Mans_Quantity = 5
+average_age = sum(Mans_Age) / Mans_Quantity
+print(average_age)
+Mans_Age = [48, 52, 64, 1, 78]
+average_age = sum(Mans_Age) / Mans_Quantity
+print(average_age)
